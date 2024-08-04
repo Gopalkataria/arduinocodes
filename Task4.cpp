82b369902730ecd993e55101a7e96bc979b7f82e
Gopal Kataria <gopalkataria@outlook.com>
2024-08-04 12:04:36 +0530

    bugfixes - Akthar

--- Task4.cpp
+++ Task4.cpp
@@ -2,7 +2,7 @@
 
 #include <ADCTouch.h>
 
-int ref0, ref1;     //reference values to remove offset
+int ref ;     //reference values to remove offset
 
 void setup() 
 {
@@ -10,19 +10,19 @@ void setup()
 
     Serial.begin(9600);
 
-    ref0 = ADCTouch.read(A0, 500);    //create reference values to 
+    ref = ADCTouch.read(A0, 500);    //create reference values to 
 } 
 
 void loop() 
 {
-    int value0 = ADCTouch.read(A0);   //no second parameter
+    int value = ADCTouch.read(A0);   //no second parameter
 
-    value0 -= ref0;       //remove offset
+    value -= ref;       //remove offset
 
     if( value > 40 ){
-        Serial.println("touchy ") ; 
+        Serial.print("touchy ") ; 
     } else {
-        Serial.println("NO") ; 
+        Serial.print("NO") ; 
     }
     Serial.print("\t");           //use if(value > threshold) to get the state of a button
 
