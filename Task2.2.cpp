597b258e2ecde929b0087705712220d479a3aa84
Gopal Kataria <gopalkataria@outlook.com>
2024-08-04 12:25:40 +0530

    Update Task2.2.cpp

--- Task2.2.cpp
+++ Task2.2.cpp
@@ -1,29 +1,19 @@
-//Task 2.2 : LDR sensor ( light dependant resistance)
-
+//Task 2.2 : IR sensor
 #define SENSOR_PIN 9
 
-void setup()
-
-{
+void setup(){
 
   pinMode(SENSOR_PIN, INPUT);
-
   Serial.begin(9600);
 
 }
 
-void loop()
-
-{
+void loop(){
 
   if (digitalRead(SENSOR_PIN) == LOW) {
-
     Serial.println("Object Detected");
-
   } else {
-
     Serial.println("Object NOT Detected");
-
   }
 
   delay(100);
