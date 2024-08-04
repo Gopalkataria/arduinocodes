d0748508cf5a3f8e50967f4664b6b7fb49f41208
Gopal Kataria <gopalkataria@outlook.com>
2024-08-04 17:14:04 +0530

    Update capbuzzer.cpp

--- capbuzzer.cpp
+++ capbuzzer.cpp
@@ -1,29 +1,20 @@
-// code for task : 4
-
 #include <ADCTouch.h>
 
-int ref ;     //reference values to remove offset
+int ref ;
 int buzzerpin = 9;
 void setup() 
 {
-    // No pins to setup, pins can still be used regularly, although it will affect readings
     Serial.begin(9600);
     pinMode(buzzerpin,OUTPUT);
-    ref = ADCTouch.read(A0, 500);    //create reference values to 
+    ref = ADCTouch.read(A0, 500); 
 } 
-
 void loop() 
 {
-    int value = ADCTouch.read(A0);   //no second parameter
-
-    value -= ref;       //remove offset
-
+    int value = ADCTouch.read(A0);   
+    value -= ref;
     if( value > 40 ){
-        // Serial.print("touchy ") ; 
         digitalWrite(buzzerpin,HIGH);
-    } else {
-        // Serial.print("NO") ; 
-        
+    } else {        
         digitalWrite(buzzerpin,LOW);
     }
 }
