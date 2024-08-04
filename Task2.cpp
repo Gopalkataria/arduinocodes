228f1d3a9448ba1b60902fdd6c757b9f60524578
Gopal Kataria <gopalkataria@outlook.com>
2024-08-04 12:24:24 +0530

    Update Task2.cpp

--- Task2.cpp
+++ Task2.cpp
@@ -1,4 +1,4 @@
-// code for task : 2 ( IR/LDR sensor )
+// code for task : 2 ( LDR sensor )
 
 const int irPin = A0;  // Analog pin connected to the IR sensor
 int irValue = 0;      // Variable to store the sensor value
