c2bf3f797c8df39e09cc2c7b2c8bd1fd12afe6f7
Gopal Kataria <gopalkataria@outlook.com>
2024-08-04 16:38:44 +0530

    Update Task2.cpp

--- Task2.cpp
+++ Task2.cpp
@@ -1,6 +1,6 @@
 // code for task : 2 ( LDR sensor )
 
-const int ldrPin = A0;  // Analog pin connected to the IR sensor
+const int ldrPin = A0;  // Analog pin connected to the LDR sensor
 int ldrValue = 0;      // Variable to store the sensor value
 
 void setup() {
@@ -8,7 +8,7 @@ void setup() {
 }
 
 void loop() {
-  ldrValue = analogRead(irPin); // Read the value from the IR sensor
+  ldrValue = analogRead(ldrPin); // Read the value from the LDR sensor
   Serial.println(ldrValue);     
   delay(500);                  
 }
